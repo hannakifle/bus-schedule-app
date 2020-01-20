185142b5d136e19eb72fdcb0ec451a3b08f744cb
hannakifle <59786011+hannakifle@users.noreply.github.com>
2020-01-20 06:20:31 -0800

    Initial commit

---
+++ busschedule app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace busschedule_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mybus = new bus();
+            mybus.busnumber = 1;
+            mybus.stationname = "A";
+            mybus.arivingtime = "8 am";
+            var mybus2 = new bus();
+            mybus2.busnumber = 2;
+            mybus2.stationname = "B";
+            mybus2.arivingtime = "1 AM";
+        }
+    }
+}
---
+++ busschedule app/bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace busschedule_app
+{
+    class bus
+    {
+        /// <summary>
+        /// this represent bus schedule
+        /// where you can get bus arriving time 
+        /// and destination time
+        /// </summary>
+        #region properties
+        public int busnumber { get; set; }
+        public string stationname { get; set; }
+        public string transit { get; set; }
+        public int arivingtime { get; set; }
+        public int destinationtime { get; set; }
+        public string direction { get; set; }
+        #endregion
+    }
+}
